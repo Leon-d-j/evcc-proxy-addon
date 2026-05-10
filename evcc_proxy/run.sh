d0a2d251362d30ae4696feb21d1be258b8a6e2c2
Leon-d-j <44633190+Leon-d-j@users.noreply.github.com>
2026-05-10 19:59:12 +0200

    Update run.sh

--- evcc_proxy/run.sh
+++ evcc_proxy/run.sh
@@ -3,14 +3,13 @@
 EVCC_IP=$(bashio::config 'evcc_ip')
 EVCC_PORT=$(bashio::config 'evcc_port')
 EVCC_URL="http://${EVCC_IP}:${EVCC_PORT}"
-INGRESS_PATH=$(bashio::addon.ingress_path)
+INGRESS_PATH=$(bashio::addon 'ingress_path')
 
 bashio::log.info "Starting evcc Proxy..."
 bashio::log.info "evcc URL: ${EVCC_URL}"
 bashio::log.info "Ingress path: ${INGRESS_PATH}"
 
-# Write nginx config with actual values
-cat > /etc/nginx/http.d/evcc_proxy.conf << EOF
+cat > /etc/nginx/http.d/evcc_proxy.conf << NGINX
 server {
     listen 8099;
 
@@ -21,7 +20,6 @@ server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
 
-        # WebSocket support (needed for evcc live updates)
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
@@ -33,7 +31,7 @@ server {
         return 301 ${INGRESS_PATH}/;
     }
 }
-EOF
+NGINX
 
 bashio::log.info "nginx config written, starting nginx..."
 nginx -g "daemon off;"
