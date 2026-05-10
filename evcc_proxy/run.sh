0321644ed860ff36230aa70810a3ac64776a7783
Leon-d-j <44633190+Leon-d-j@users.noreply.github.com>
2026-05-10 20:01:55 +0200

    Update run.sh

--- evcc_proxy/run.sh
+++ evcc_proxy/run.sh
@@ -3,17 +3,15 @@
 EVCC_IP=$(bashio::config 'evcc_ip')
 EVCC_PORT=$(bashio::config 'evcc_port')
 EVCC_URL="http://${EVCC_IP}:${EVCC_PORT}"
-INGRESS_PATH=$(bashio::addon 'ingress_path')
 
 bashio::log.info "Starting evcc Proxy..."
 bashio::log.info "evcc URL: ${EVCC_URL}"
-bashio::log.info "Ingress path: ${INGRESS_PATH}"
 
 cat > /etc/nginx/http.d/evcc_proxy.conf << NGINX
 server {
     listen 8099;
 
-    location ${INGRESS_PATH}/ {
+    location / {
         proxy_pass ${EVCC_URL}/;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
@@ -25,10 +23,6 @@ server {
         proxy_set_header Connection "upgrade";
 
         proxy_read_timeout 86400;
-    }
-
-    location ${INGRESS_PATH} {
-        return 301 ${INGRESS_PATH}/;
     }
 }
 NGINX
